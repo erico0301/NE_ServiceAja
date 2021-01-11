7269480f0e6ba1963e13bf519f214965e2a89466
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-01-12 00:40:27 +0700

    Update HomeActivity.kt

--- ServiceAja/app/src/main/java/com/example/serviceaja/HomeActivity.kt
+++ ServiceAja/app/src/main/java/com/example/serviceaja/HomeActivity.kt
@@ -18,7 +18,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        replaceFragment(profileFragment)
         replaceFragment(homeFragment)
 
         bottomNavBarMenu.setOnNavigationItemSelectedListener {
