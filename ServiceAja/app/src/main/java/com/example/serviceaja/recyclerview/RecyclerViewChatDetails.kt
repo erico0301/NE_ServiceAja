d569936a0b70a92acaaccc6e647c99883e929c09
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-01-13 23:34:12 +0700

    Update RecyclerViewChatDetails.kt

--- ServiceAja/app/src/main/java/com/example/serviceaja/recyclerview/RecyclerViewChatDetails.kt
+++ ServiceAja/app/src/main/java/com/example/serviceaja/recyclerview/RecyclerViewChatDetails.kt
@@ -9,10 +9,13 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.serviceaja.R
 import com.example.serviceaja.chatreview.ChatActivity
+import com.example.serviceaja.transaction.OnGoingTransactionActivity
+import kotlinx.android.synthetic.main.recyclerview_chat_details.view.*
 
 class RecyclerViewChatDetails : RecyclerView.Adapter<RecyclerViewChatDetails.ViewHolder>() {
 
     private val itemImgBengkel = intArrayOf(R.drawable.mitsubishi_logo, R.drawable.bmw_logo, R.drawable.peugeot_logo)
+    private val itemImgBengkel = intArrayOf(R.drawable.mitsubishi_logo, R.drawable.bmw_logo_1, R.drawable.peugeot_logo)
     private val itemNamaBengkel = arrayOf("Mitsubishi", "BMW", "Peugeot")
     private val itemLastMessage = arrayOf("Terima kasih!", "Oke", "Harga OTR Medan")
     private val itemLastHour = arrayOf("10:32 pm", "10:23 am", "23 Des 20")
@@ -45,6 +48,9 @@ class RecyclerViewChatDetails : RecyclerView.Adapter<RecyclerViewChatDetails.Vie
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         var view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recyclerview_chat_details, parent, false)
+        view.setOnClickListener{
+            parent.context.startActivity(Intent(parent.context, ChatActivity::class.java))
+        }
         return ViewHolder(view)
     }
 
@@ -54,6 +60,13 @@ class RecyclerViewChatDetails : RecyclerView.Adapter<RecyclerViewChatDetails.Vie
         holder.lastMessage.text = itemLastMessage[position]
         holder.lastHour.text = itemLastHour[position]
         holder.totalUnreadMessage.text = itemTotalUnreadMessage[position]
+        holder.itemView.apply {
+            bengkelImg.setImageResource(itemImgBengkel[position])
+            namaMitra.text = itemNamaBengkel[position]
+            lastMessage.text = itemLastMessage[position]
+            lastMessageHour.text = itemLastHour[position]
+            totalUnreadMessage.text = itemTotalUnreadMessage[position]
+        }
     }
 
     override fun getItemCount(): Int {
