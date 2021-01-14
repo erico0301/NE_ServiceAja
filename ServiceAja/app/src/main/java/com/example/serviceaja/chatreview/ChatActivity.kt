b1502d796e33b05baaa7f410f3295f898ecc92b8
erico0301 <76524313+erico0301@users.noreply.github.com>
2021-01-14 22:28:33 +0700

    Update ChatActivity.kt

--- ServiceAja/app/src/main/java/com/example/serviceaja/chatreview/ChatActivity.kt
+++ ServiceAja/app/src/main/java/com/example/serviceaja/chatreview/ChatActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.serviceaja.R
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.activity_on_going_transaction.*
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
